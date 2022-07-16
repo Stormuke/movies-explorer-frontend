b1894b0bed0993eb24621f41b95973590c39da22
stormuke <fen1x@yandex.ru>
2022-07-16 22:48:57 +0300

    feat(SearchForm): написал логику сабмита, передлал поиск на форму, добавил логику свитчу

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,16 +1,27 @@
 import "./SearchForm.css";
-import React from "react";
+import React, {useState} from "react";
 
-function SearchForm() {
+function SearchForm({handleSearch, durationFilter}) {
+  const [checked, setChecked] = useState(false)
+  const [value, setValue] = useState('')
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault()
+
+    handleSearch(value)
+  }
 
   return (
     <section className="search-form">
-      <div className="search-form__container">
-        <input className="search-form__input" placeholder="Фильм" id="queryInput" required={true}/>
-        <button className="search-form__button"/>
-      </div>
+      <form className="search-form__container" onSubmit={(e) => handleSubmitForm(e)}>
+        <input className="search-form__input" value={value} placeholder="Фильм" id="queryInput" onChange={(e) => setValue(e.target.value)}/>
+        <button type='submit' className="search-form__button"/>
+      </form>
       <div className="search-form__wrapper">
-        <button className="search-form__toggle"/>
+        <button type='button' className={`search-form__toggle search-form__toggle${checked ? '_on' : "_off"}`} onClick={() => {
+          setChecked(!checked)
+          durationFilter(checked)
+        }}/>
         <p className="search-form__text">Короткометражки</p>
       </div>
     </section>
