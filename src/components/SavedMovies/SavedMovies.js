9406617f70c2b9e17ee19a42bc4d3bb0d4a40957
stormuke <fen1x@yandex.ru>
2022-07-16 22:50:31 +0300

    feat(SavedMovies): прокинул пропсы в компонент

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -2,12 +2,12 @@ import MoviesCards from "../MoviesCards/MoviesCards";
 import SearchForm from "../SearchForm/SearchForm";
 import React from "react";
 
-function SavedMovies({movieCards, onDelete}) {
+function SavedMovies({movieCards, onDelete, listLength, handleSearch, durationFilter}) {
 
   return (
     <>
-      <SearchForm/>
-      <MoviesCards movieCards={movieCards} onDelete={onDelete}/>
+      <SearchForm handleSearch={handleSearch} durationFilter={durationFilter}/>
+      <MoviesCards movieCards={movieCards} onDelete={onDelete} listLength={listLength}/>
     </>
   )
 }
