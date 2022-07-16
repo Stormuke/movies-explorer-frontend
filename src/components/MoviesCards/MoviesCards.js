56d446a0320e34e1ee850903011e7df53002f604
stormuke <fen1x@yandex.ru>
2022-07-16 22:52:29 +0300

    feat(MoviesCards): сделал отрисовку карточек по ширине экрана, добавил ui пустого запроса

--- src/components/MoviesCards/MoviesCards.js
+++ src/components/MoviesCards/MoviesCards.js
@@ -1,17 +1,23 @@
 import MoviesCard from "../MoviesCard/MoviesCard";
 import "./MoviesCards.css"
+import Preloader from "../Preloader/Preloader";
 
-function MoviesCards({movieCards, onSave, onDelete, savedMovies}) {
+function MoviesCards({movieCards, onSave, onDelete, savedMovies, listLength, addMovies}) {
   return (
-    <section className="cards">
-      {movieCards.map((card, id ) => {
-        return (
-          <div className="cards__container" key={card.id ? card.id : id}>
-            <MoviesCard card={card} isLiked={card.isLiked} onSave={onSave} onDelete={onDelete} savedMovies={savedMovies}/>
-          </div>
+    <section className="container">
+      <div className="cards">
+        {movieCards.map((card, id ) => {
+          return (
+            <div className="cards__container" key={card.id ? card.id : id}>
+              <MoviesCard card={card} isLiked={card.isLiked} onSave={onSave} onDelete={onDelete} savedMovies={savedMovies}/>
+            </div>
 
-        )
-      })}
+          )
+        }).slice(0, listLength)}
+      </div>
+
+
+      {movieCards.length === 0 ? <p>Введите название фильма в поисковой строке</p> : movieCards.length > listLength && <Preloader addMovies={addMovies}/>}
     </section>
   )
 }
