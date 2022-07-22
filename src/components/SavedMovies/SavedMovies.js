d6f79f995d58329f51228d9129f97e923e83276e
stormuke <fen1x@yandex.ru>
2022-07-22 21:36:20 +0300

    feat(SavedMovies): прокинул логику кнопки еще

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -2,12 +2,12 @@ import MoviesCards from "../MoviesCards/MoviesCards";
 import SearchForm from "../SearchForm/SearchForm";
 import React from "react";
 
-function SavedMovies({movieCards, onDelete, listLength, handleSearch, durationFilter}) {
+function SavedMovies({movieCards, onDelete, listLength, handleSearch, durationFilter, addMovies}) {
 
   return (
     <>
       <SearchForm handleSearch={handleSearch} durationFilter={durationFilter}/>
-      <MoviesCards movieCards={movieCards} onDelete={onDelete} listLength={listLength}/>
+      <MoviesCards movieCards={movieCards} onDelete={onDelete} listLength={listLength} addMovies={addMovies}/>
     </>
   )
 }
