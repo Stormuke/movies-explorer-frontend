d9d26f8bce3e15fef0c2c08bd24932ad0d79f96c
stormuke <fen1x@yandex.ru>
2022-07-22 21:31:33 +0300

    feat(formValidator): сделал валидацию почты по регулярке

--- src/utils/formValidator.js
+++ src/utils/formValidator.js
@@ -1,4 +1,4 @@
-import {useCallback, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 export function useForm() {
   const [values, setValues] = useState({});
@@ -17,6 +17,15 @@ export function useFormWithValidation() {
   const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
   const [isValid, setIsValid] = useState(false);
+  const [targetValue, setTargetValue] = useState('')
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
   const handleChange = (event) => {
     const target = event.target;
@@ -24,8 +33,13 @@ export function useFormWithValidation() {
     const value = target.value;
     setValues({...values, [name]: value});
     setErrors({...errors, [name]: target.validationMessage});
-    setIsValid(target.closest("form").checkValidity());
+    setTargetValue(target)
   };
+
+  useEffect(() => {
+    setIsValid((Boolean(values.email && validateEmail(values.email))) && targetValue.checkValidity())
+  }, [values, targetValue])
+
 
   const resetForm = useCallback(
     (newValues = {}, newErrors = {}, newIsValid = false) => {
