35e6a37bba1071e09cf41ad74d4698f646ceedb6
stormuke <fen1x@yandex.ru>
2022-07-24 13:25:41 +0300

    feat(Profile): добавил дизейбл кнопки редактировать, если нет изменений

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,14 @@
 import "./Profile.css";
 import {CurrentUserContext} from "../../utils/CurrentUserContext";
-import {useContext, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 
 
 function Profile({signOut, onSubmit}) {
   const user = useContext(CurrentUserContext);
   const [userName, setUserName] = useState(user.name);
   const [userEmail, setUserEmail] = useState(user.email);
+  const [disableForm, setDisableForm] = useState(true)
+
 
   function handleChangeName(evt) {
     setUserName(evt.target.value);
@@ -21,6 +23,15 @@ function Profile({signOut, onSubmit}) {
     onSubmit({name: userName, email: userEmail})
   }
 
+  useEffect(() => {
+    if (userName !== user.name || userEmail !== user.email) {
+      setDisableForm(false)
+    }
+     else {
+      setDisableForm(true)
+    }
+  }, [handleChangeName, handleChangeEmail, userName, userEmail])
+
   return (
     <section className="profile">
       <div className="profile__container">
@@ -30,15 +41,15 @@ function Profile({signOut, onSubmit}) {
             <label className="profile__label">
               Имя
             </label>
-            <input className="profile__input" value={userName} onChange={handleChangeName}/>
+            <input className="profile__input" value={userName} onChange={handleChangeName} name='name' type='text'/>
           </div>
           <div className="profile__form-input">
             <label className="profile__label">
               E-mail
             </label>
-            <input className="profile__input" value={userEmail} onChange={handleChangeEmail}/>
+            <input className="profile__input" value={userEmail} onChange={handleChangeEmail} name='email' type='email'/>
           </div>
-          <button className="profile__submit" type='submit'>Редактировать</button>
+          <button className={`profile__submit ${disableForm && "profile__submit_disabled"}`} type='submit' disabled={disableForm}>Редактировать</button>
         </form>
         <button className="profile__signout" onClick={signOut}>Выйти из аккаунта</button>
       </div>
