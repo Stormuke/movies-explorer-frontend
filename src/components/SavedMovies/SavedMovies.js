6f909fa88f2fd0de6a1ff0d70e9f47d67bb8689e
stormuke <fen1x@yandex.ru>
2022-07-24 21:09:48 +0300

    feat(SavedMovies): добавил удаление

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -2,12 +2,11 @@ import MoviesCards from "../MoviesCards/MoviesCards";
 import SearchForm from "../SearchForm/SearchForm";
 import React from "react";
 
-function SavedMovies({movieCards, onDelete, listLength, handleSearch, durationFilter, addMovies}) {
-
+function SavedMovies({movieCards, onDelete, listLength, handleSearch, durationFilter, addMovies, savedMovies}) {
   return (
     <>
       <SearchForm handleSearch={handleSearch} durationFilter={durationFilter}/>
-      <MoviesCards movieCards={movieCards} onDelete={onDelete} listLength={listLength} addMovies={addMovies}/>
+      <MoviesCards movieCards={movieCards} onDelete={onDelete} listLength={listLength} addMovies={addMovies} savedMovies={savedMovies}/>
     </>
   )
 }
