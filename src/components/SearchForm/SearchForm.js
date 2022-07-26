a884246dfa68f3d6f1f8ac1869879e3f88fd1ed7
stormuke <fen1x@yandex.ru>
2022-07-26 23:41:30 +0300

    fix(SearchForm): переделал логику чекбоксов и поисковой строки, добавил в локал сторадж данные

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,22 +3,44 @@ import React, {useEffect, useState} from "react";
 import {useLocation} from "react-router-dom";
 
 function SearchForm({handleSearch, durationFilter}) {
-  const [checked, setChecked] = useState(false)
-  const [value, setValue] = useState('')
+  const localStorageValue = localStorage.getItem('savedSearchValue')
+  const localChecked = localStorage.getItem('savedCheck')
 
+  const [checked, setChecked] = useState(localChecked ?? '0')
+  const [value, setValue] = useState(localStorageValue ?? '')
   const location = useLocation()
 
   const handleSubmitForm = (e) => {
     e.preventDefault()
+    setChecked('0')
     handleSearch(value)
   }
 
   useEffect(() => {
     if (location.pathname === '/saved-movies') {
+      setChecked('0')
       handleSearch(value)
       setValue('')
     }
   }, [location])
+
+  useEffect(() => {
+    if (location.pathname === '/movies') {
+      localStorage.setItem('savedSearchValue', value)
+      localStorage.setItem('savedCheck', checked)
+    }
+  }, [value, checked])
+
+  useEffect(() => {
+    if (location.pathname === '/saved-movies') {
+      durationFilter(checked)
+    }
+    if (location.pathname === '/movies') {
+      handleSearch(localStorageValue ?? '')
+      durationFilter(checked ?? '0')
+    }
+  }, [location, checked])
+
 
   return (
     <section className="search-form">
@@ -28,9 +50,9 @@ function SearchForm({handleSearch, durationFilter}) {
         <button type='submit' className="search-form__button"/>
       </form>
       <div className="search-form__wrapper">
-        <button type='button' className={`search-form__toggle search-form__toggle${checked ? '_on' : "_off"}`}
+        <button type='button' className={`search-form__toggle search-form__toggle${checked === '1' ? '_on' : "_off"}`}
                 onClick={() => {
-                  setChecked(!checked)
+                  setChecked(checked === '0' ? '1' : '0')
                   durationFilter(checked)
                 }}/>
         <p className="search-form__text">Короткометражки</p>
